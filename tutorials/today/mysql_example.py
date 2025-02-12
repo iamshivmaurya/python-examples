4b115a2e67d9c2558af814c00d440dbd7ba6f6d9
shiv <skmaurya.ald@gmail.com>
2025-02-12 23:01:43 +0530

    comment added for mysql connection

--- tutorials/today/mysql_example.py
+++ tutorials/today/mysql_example.py
@@ -1,12 +1,13 @@
 import mysql.connector
-
+# mysql connection 
+# mysql module for connect with mysql database
 connection = mysql.connector.connect(
     host="bannerbuzz246-db-1",
     user="root",
     password="root123",
     database="election"
 )
-
+# cursor for query with database
 cursor = connection.cursor()
 
 #query = "SELECT * FROM users WHERE 1"
@@ -15,7 +16,7 @@ users = cursor.fetchall()
 
 
 for row in users:
-    print(row)
+    #print(row)
 
 cursor.close()
 connection.close()
